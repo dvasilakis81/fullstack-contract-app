dc3a655626cadbab3ff4f2f4d9da3923c541cf80
dvasilakis <dvasilakis81@gmail.com>
2020-04-05 18:38:09 +0300

    a

--- client/src/Redux/Reducers/token_reducer.js
+++ client/src/Redux/Reducers/token_reducer.js
@@ -1,19 +1,23 @@
 export default function (state = {}, action) {
 
-  switch (action.type) {    
+  switch (action.type) {
     case 'RESET_ACTION':
       state = {}
       break;
     case 'GET_TOKEN_JWT_PENDING':      
+       console.log('TOKEN_REDUCER: GET_TOKEN_JWT_PENDING')
       state = { ...state, tokenPending: 'token pending', tokenRejected: undefined, token: undefined };
       break;
     case 'GET_TOKEN_JWT_FULFILLED':      
+      console.log('TOKEN_REDUCER: GET_TOKEN_JWT_FULFILLED')
       state = { ...state, tokenPending: undefined, tokenRejected: undefined, token: action.payload };
       break;
     case 'GET_TOKEN_JWT_REJECTED':      
+      console.log('TOKEN_REDUCER: GET_TOKEN_JWT_REJECTED')
       state = { ...state, tokenPending: undefined, tokenRejected: action.payload };
       break;
-    case 'RESET_TOKEN_JWT_REJECTED':      
+    case 'RESET_TOKEN_JWT_REJECTED':
+      console.log('TOKEN_REDUCER: RESET_TOKEN_JWT_REJECTED')
       state = { ...state, tokenPending: undefined, tokenRejected: action.payload };
       break;    
     default:      
